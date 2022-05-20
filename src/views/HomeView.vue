b65fcc16072ee1686260406f6b1539eeaa72002e
ProjectM431 <m431project@gmail.com>
2022-05-20 10:46:01 +0200

    add tailwindcss and tailwindcssforms

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="bg-blue-500"> C'estDEhors</p>
   </div>
 </template>
 
