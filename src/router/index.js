bca1b50b044a14c3bd3888a7f8bc784f209e80ee
ribbi <theo.ribbi@palexpo.ch>
2022-05-31 09:38:46 +0200

    add new components teacher

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AccountLogin.vue')
-  },
+const routes = [{
+        path: '/login',
+        name: 'login',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AccountLogin.vue')
+    },
+    {
+        path: '/', // Redirection, si l'URL est vide alors redirection vers la page login
+        redirect: { name: 'login' }
+    },
+    {
+        path: '/teacher',
+        name: 'teacher',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/TeacherViews.vue')
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
