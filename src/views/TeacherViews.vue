780a6939e328cddd99492441b99e29133b9eed5d
ribbi <theo.ribbi@palexpo.ch>
2022-06-13 21:56:50 +0200

    add post api for appreciation

--- src/views/TeacherViews.vue
+++ src/views/TeacherViews.vue
@@ -8,7 +8,7 @@
             <div class="flex-1 flex flex-col overflow-hidden">
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
                     <div class="container mx-auto px-6 py-8">
-                        <h3 class="text-gray-700 text-3xl font-medium">Gestion</h3>
+                        <h3 class="text-gray-700 text-3xl font-medium">Bienvenue {{userInfo.first_name}} </h3>
                         <div class="mt-4">
                             <div class="flex flex-wrap -mx-6">
                                 <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
@@ -24,39 +24,8 @@
                                             </svg>
                                         </div>
                                         <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">2</h4>
+                                            <h4 class="text-2xl font-semibold text-gray-700">{{ userList.length }} </h4>
                                             <div class="text-gray-500">Eleves</div>
-                                        </div>
-                                    </div>
-                                </div>
-        
-                                <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
-                                    <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
-                                        <div class="p-3 rounded-full bg-orange-600 bg-opacity-75">
-                                            <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M4.19999 1.4C3.4268 1.4 2.79999 2.02681 2.79999 2.8C2.79999 3.57319 3.4268 4.2 4.19999 4.2H5.9069L6.33468 5.91114C6.33917 5.93092 6.34409 5.95055 6.34941 5.97001L8.24953 13.5705L6.99992 14.8201C5.23602 16.584 6.48528 19.6 8.97981 19.6H21C21.7731 19.6 22.4 18.9732 22.4 18.2C22.4 17.4268 21.7731 16.8 21 16.8H8.97983L10.3798 15.4H19.6C20.1303 15.4 20.615 15.1004 20.8521 14.6261L25.0521 6.22609C25.2691 5.79212 25.246 5.27673 24.991 4.86398C24.7357 4.45123 24.2852 4.2 23.8 4.2H8.79308L8.35818 2.46044C8.20238 1.83722 7.64241 1.4 6.99999 1.4H4.19999Z" fill="currentColor"></path>
-                                                <path d="M22.4 23.1C22.4 24.2598 21.4598 25.2 20.3 25.2C19.1403 25.2 18.2 24.2598 18.2 23.1C18.2 21.9402 19.1403 21 20.3 21C21.4598 21 22.4 21.9402 22.4 23.1Z" fill="currentColor"></path>
-                                                <path d="M9.1 25.2C10.2598 25.2 11.2 24.2598 11.2 23.1C11.2 21.9402 10.2598 21 9.1 21C7.9402 21 7 21.9402 7 23.1C7 24.2598 7.9402 25.2 9.1 25.2Z" fill="currentColor"></path>
-                                            </svg>
-                                        </div>
-                                        <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">800</h4>
-                                            <div class="text-gray-500">Aasdasdasd</div>
-                                        </div>
-                                    </div>
-                                </div>
-        
-                                <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
-                                    <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
-                                        <div class="p-3 rounded-full bg-pink-600 bg-opacity-75">
-                                            <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linejoin="round"></path>
-                                                <path d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z" stroke="currentColor" stroke-width="2"></path>
-                                            </svg>
-                                        </div>
-                                        <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">800</h4>
-                                            <div class="text-gray-500">Adad2ddasd</div>
                                         </div>
                                     </div>
                                 </div>
@@ -99,15 +68,15 @@
         
                                                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                                     <div class="text-sm leading-5 text-gray-900"></div>
-                                                    <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                                    <option v-for="observationList in observationList" :key="observationList.id">{{observationList.title}}</option>
+                                                    <select v-model="observationSelected" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                    <option v-for="observationList in observationList" v-bind:key="observationList.id">{{observationList.title}}</option>
                                                     </select>
                                                 </td>
                                                 <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                                   <input type="datetime-local" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                   <input v-model="observationDate" type="datetime-local" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                                 </td>
                                                 <td class="px-6 py-4 whitespace-no-wrap border-b flex justify-center border-gray-200">
-                                                   <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                                   <button @click="sendAppreciation(userList)" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                                                         Envoyez
                                                     </button>
                                                 </td>
@@ -134,23 +103,78 @@ export default {
   data: () => ({
     isLogged: null,
     token: null,
+    user_id_cache: null,
+    user_first_name_cache: null,
+    user_last_name_cache: null,
+    userInfo: {},
     userList: {},
     observationList: {},
+    selectedList: {},
+    observationSelected: null,
+    username: null,
+    observationDate: null,
     apiUser: "http://127.0.0.1:8003/profile/user",
+    apiUserId: "http://127.0.0.1:8003/profile/user/",
     apiObservation: "http://127.0.0.1:8003/observation",
+    apiAppreciation: "http://127.0.0.1:8003/appreciations",
   }),
 
   methods: {
+
+    sendAppreciation(userList){
+
+    if (this.observationSelected == "Absence non excusée") {
+        this.observationSelected = 3
+    } 
+
+    if (this.observationSelected == "Absence Excusée") {
+        this.observationSelected = 4
+    } 
+
+    if (this.observationSelected == "Arrivé Tardive") {
+        this.observationSelected = 5
+    } 
+
+    if (this.observationSelected == "Renvoi") {
+        this.observationSelected = 6
+    } 
+
+    if (this.observationSelected == "Devoir non fait") {
+        this.observationSelected = 7
+    } 
     
+    if (this.observationSelected == "Oublie") {
+        this.observationSelected = 8
+    } 
+
+    var userInfoAPI = {
+        date_time: this.observationDate,
+        appreciation: this.observationSelected,
+        user: userList.id
+    }
+
+    console.log(userInfoAPI)
+   
+   console.log(this.observationSelected)
+
+    axios.post(this.apiAppreciation, userInfoAPI).then(response => { 
+      console.log('response');
+      console.log(response);
+    });
+    },
+
   },
 
   mounted() {
+    this.token = localStorage.getItem('token'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
+    this.user_id_cache = localStorage.getItem('user.id'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
+    this.user_first_name_cache = localStorage.getItem('user.first_name'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
+    this.user_last_name_cache = localStorage.getItem('user.last_name'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
     axios.get(this.apiUser).then((response) => (this.userList = response.data)); // API User
     axios.get(this.apiObservation).then((response) => (this.observationList = response.data)); // API Observation
-    this.token = localStorage.getItem('token'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
-    this.token = localStorage.getItem('user.id'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
-    this.token = localStorage.getItem('user.first_name'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
-    this.token = localStorage.getItem('user.last_name'); // Token de l'API d'authentification (Comp. AccountLogin.vue) et le stock dans le navigateur
+    axios.get(this.apiUserId + this.user_id_cache).then((response) => (this.userInfo = response.data)); // API UserID
+    
+   
   },
 };
 </script>
